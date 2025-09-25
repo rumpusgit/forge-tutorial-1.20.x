23d503047b88a35a27b72bf6e453631a5e36fe1d
Jack Hamilton <hamilt24@sheridancollege.ca>
2025-09-25 17:27:29 -0400

    I added a space

--- src/main/java/net/seaja/tutorialmod/TutorialMod.java
+++ src/main/java/net/seaja/tutorialmod/TutorialMod.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
